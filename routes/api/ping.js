e7c99b1dfd7afca9f5e4de7ac6bfe1dde4484e7d
Brandon Sedgwick <bmsedgwick@gmail.com>
2018-02-10 15:05:59 -0500

    Update ping status codes (#3)
    * Adds ping endpoint for healthcheck
    
    * Update package.json for tests to pass
    
    * Update ejs due to security vulnerability
    
    * Update license and repo url
    
    * Update the ping status code responses

--- routes/api/ping.js
+++ routes/api/ping.js
@@ -3,10 +3,25 @@ var mongoose = require('mongoose');
 
 // return api status
 router.get('/', function(req, res, next) {
-  return res.json({
-      // http://mongoosejs.com/docs/api.html#connection_Connection-readyState
-      db_state: mongoose.connection.readyState
-  });
+  var status = 500;
+  var message = "";
+  // http://mongoosejs.com/docs/api.html#connection_Connection-readyState
+  if (mongoose.connection.readyState === 1) {
+    status = 200;
+    message = "Success";
+  } else {
+    switch (mongoose.connection.readyState) {
+      case 0: message = "Database disconnected";
+      case 1: message = "Database connected"; // should not reach
+      case 2: message = "Database connecting";
+      case 3: message = "Database disconnecting";
+      default: message = "Unknown error with database";
+    }
+  }
+
+  return res.status(status).json({
+      message: message
+    });
 });
 
 module.exports = router;
